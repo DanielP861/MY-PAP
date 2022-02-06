c1941b3e53f8b7c86ca7193138dd84c084bcd7e0
DanielP861 <danielponte861@gmail.com>
2022-02-06 20:28:40 -0100

    pap

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ app.use(express.static('./public'))
 app.get('/',  (req, res) => {
   res.sendFile(path.join(__dirname,'./public/index.html'))
 })
+
+
  
+
 const port = 3000
 
 app.listen(port, () => {
